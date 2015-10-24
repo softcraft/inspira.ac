4b47b7ea9bf7aa6ffa9b72424c84e01c43c65a5a
Enrique Ramírez <me@enrique-ramirez.com>
2015-10-24 12:43:26 -0500

    A VER SI SÍCIERTO

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ get_header(); ?>
     <div class="home-search">
         <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
             <label>
+                <input type="hidden" name="post_type" value="post" />
                 <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
             </label>
         </form>
