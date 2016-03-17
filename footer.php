94201200c48d7e940baeab1dcbea34ee2737f8cc
Javier Cervantes <javier@sawyereffect.com>
2016-03-17 17:47:35 -0600

    Better footer style

--- footer.php
+++ footer.php
@@ -43,7 +43,7 @@
             <div id="mc_embed_signup">
               <form action="//inspira.us7.list-manage.com/subscribe/post?u=0367bbfc22583f07097f56d65&amp;id=88ea1a39f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
               <div id="mc_embed_signup_scroll">
-              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Correo electr&oactute;nico" required>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Correo electrónico" required>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0367bbfc22583f07097f56d65_88ea1a39f1" tabindex="-1" value=""></div>
               <div class="clear"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
