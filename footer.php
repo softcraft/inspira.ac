55f4bf040078513524d2bfc412c9cdb2d14cf5b5
Javier Cervantes <javier@sawyereffect.com>
2016-03-17 17:42:56 -0600

    Update styles for footer form

--- footer.php
+++ footer.php
@@ -39,15 +39,10 @@
 
         <div class="subscribe">
             Suscríbete a nuestra red de Inspiración
-            <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
-            <style type="text/css">
-              #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-            </style>
             <div id="mc_embed_signup">
               <form action="//inspira.us7.list-manage.com/subscribe/post?u=0367bbfc22583f07097f56d65&amp;id=88ea1a39f1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
               <div id="mc_embed_signup_scroll">
-              <label for="mce-EMAIL">Subscribe to our mailing list</label>
-              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Correo electr&oactute;nico" required>
               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
               <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0367bbfc22583f07097f56d65_88ea1a39f1" tabindex="-1" value=""></div>
               <div class="clear"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
