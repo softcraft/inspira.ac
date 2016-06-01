c55c705369000d50d4e3ed6de207dd1da4a5b6b7
Javier Cervantes <javier@sawyereffect.com>
2016-05-31 21:14:37 -0500

    Add hreflang

--- header.php
+++ header.php
@@ -12,6 +12,8 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+    <link rel="alternate" href="http://inspira.ac" hreflang="es-mx" />
+    <link rel="alternate" href="http://www.inspira.ac" hreflang="es-mx" />
     <!--[if lt IE 9]>
     <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
     <![endif]-->
